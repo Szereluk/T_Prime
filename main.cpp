d53be6d30469d68606fa76f33194d25e302809c9
szereluk <sebastian.szereluk@gmail.com>
2017-10-18 21:58:31 +0200

    main v2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ int czyPierwsza(int a);
 int main()
 {
 
+    int n,a;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        if(czyPierwsza(a))
+            cout<<"TAK"<<endl;
+        else
+            cout<<"NIE"<<endl;
+    }
 
+
+    return 0;
 }
 int czyPierwsza(int a)
 {
